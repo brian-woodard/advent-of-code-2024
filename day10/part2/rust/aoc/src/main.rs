1f6e2cdcc0275da0e1edbf981842a1300c985a31
Brian Woodard <brian.woodard@everusengineering.com>
2025-02-17 08:45:03 -0500

    Finish Day 10 Part 2 Rust

--- day10/part2/rust/aoc/src/main.rs
+++ day10/part2/rust/aoc/src/main.rs
@@ -1,11 +1,13 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::collections::HashMap;
 
 fn main() {
-    let file = File::open("../../test.txt").expect("failed to open file");
+    let file = File::open("../../input.txt").expect("failed to open file");
     let reader = BufReader::new(file);
     let mut input: Vec<i32> = Vec::new();
+    let mut trailheads: HashMap<usize, HashMap<usize, i32>> = HashMap::new();
     let mut width: i32 = 0;
     let mut height: i32 = 0;
     let mut result: u64 = 0;
@@ -29,7 +31,19 @@ fn main() {
 
     for i in 0..input.len() {
         if input[i] == 0 {
-            println!("check_trailhead");
+            if let Some(_trailhead) = trailheads.get_mut(&i) {
+                // do nothing
+            } else {
+                trailheads.insert(i, HashMap::new());
+            }
+            check_trailhead(&input, i as i32, -1, width, height, trailheads.get_mut(&i).unwrap());
+
+            for trailhead in trailheads.get(&i) {
+                for score in trailhead.values() {
+                   //println!("{}: {:?}", i, score);
+                   result += *score as u64;
+                }
+            }
         }
     }
 
@@ -45,4 +59,46 @@ fn print_map(map: &Vec<i32>, width: i32, height: i32) {
         println!();
     }
     println!();
+}
+
+fn check_trailhead(map: &Vec<i32>, idx: i32, prev_score: i32, width: i32, height: i32, trailhead: &mut HashMap<usize, i32>) {
+    let x = idx % width;
+    let y = idx / width;
+    let mut score = map[idx as usize] as i32;
+
+    if x < 0 || x >= width || y < 0 || y >= height {
+        return;
+    }
+
+    if score != prev_score + 1 {
+        return;
+    }
+
+    if score == 9 {
+        if let Some(trailhead_score) = trailhead.get_mut(&(idx as usize)) {
+            *trailhead_score += 1;
+        } else {
+            trailhead.insert(idx as usize, 1);
+        }
+    }
+
+    // Check up
+    if y > 0 {
+        check_trailhead(map, idx - width, score, width, height, trailhead);
+    }
+
+    // Check down
+    if y < height - 1 {
+        check_trailhead(map, idx + width, score, width, height, trailhead);
+    }
+
+    // Check right
+    if x < width - 1 {
+        check_trailhead(map, idx + 1, score, width, height, trailhead);
+    }
+
+    // Check left
+    if x > 0 {
+        check_trailhead(map, idx - 1, score, width, height, trailhead);
+    }
 }
